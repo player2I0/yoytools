f6e5179c7d1450417606081e83fae2cd2aaf540b
player210 <59975613+player2I0@users.noreply.github.com>
2024-08-30 18:04:53 +0200

    removed 'player' parameter from units (because unit's 'owner' is described with hex' owner on which it is standing)

--- src/yoytools/game/world/units.py
+++ src/yoytools/game/world/units.py
@@ -2,49 +2,48 @@ class Unit:
     """
     Base class for describing a game unit
     """
-    def __init__(self, hex = None, player = None):
+    def __init__(self, hex = None):
         self.hex = hex
-        self.player = player
 
 
 class Capital(Unit):
     """
     The capital of a player (this annoying golden castle)
     """
-    def __init__(self, hex = None, player = None):
-        super().__init__(hex, player)
+    def __init__(self, hex = None):
+        super().__init__(hex)
 
 
 class Peasant(Unit):
     """
     A human
     """
-    def __init__(self, hex = None, player = None):
-        super().__init__(hex, player)
+    def __init__(self, hex = None):
+        super().__init__(hex)
 
 
 class Warrior(Peasant):
     """
     A human with a spear.
     """
-    def __init__(self, hex = None, player = None):
-        super().__init__(hex, player)
+    def __init__(self, hex = None):
+        super().__init__(hex)
 
 
 class Squire(Peasant):
     """
     A human in red armor
     """
-    def __init__(self, hex = None, player = None):
-        super().__init__(hex, player)
+    def __init__(self, hex = None):
+        super().__init__(hex)
 
 
 class Knight(Peasant):
     """
     A human in red armor, shield and sword
     """
-    def __init__(self, hex = None, player = None):
-        super().__init__(hex, player)
+    def __init__(self, hex = None):
+        super().__init__(hex)
 
 
 class Tower(Unit):
