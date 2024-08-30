097f153815ce157ad6e8bc98ca749e7dff6de4af
player210 <59975613+player2I0@users.noreply.github.com>
2024-08-30 17:58:00 +0200

    added basic classes describing game units

--- src/yoytools/game/world/units.py
+++ src/yoytools/game/world/units.py
@@ -8,5 +8,56 @@ class Unit:
 
 
 class Capital(Unit):
+    """
+    The capital of a player (this annoying golden castle)
+    """
     def __init__(self, hex = None, player = None):
-        super.__init__(hex, player)
+        super().__init__(hex, player)
+
+
+class Peasant(Unit):
+    """
+    A human
+    """
+    def __init__(self, hex = None, player = None):
+        super().__init__(hex, player)
+
+
+class Warrior(Peasant):
+    """
+    A human with a spear.
+    """
+    def __init__(self, hex = None, player = None):
+        super().__init__(hex, player)
+
+
+class Squire(Peasant):
+    """
+    A human in red armor
+    """
+    def __init__(self, hex = None, player = None):
+        super().__init__(hex, player)
+
+
+class Knight(Peasant):
+    """
+    A human in red armor, shield and sword
+    """
+    def __init__(self, hex = None, player = None):
+        super().__init__(hex, player)
+
+
+class Tower(Unit):
+    """
+    A tower.
+    """
+    def __init__(self, hex = None):
+        super().__init__(hex, None)
+
+
+class UpgradedTower(Tower):
+    """
+    An upgraded tower.
+    """
+    def __init__(self, hex = None):
+        super().__init__(hex)
